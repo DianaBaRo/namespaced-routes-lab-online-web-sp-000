7d710a6ade857ecb66eff11120c936583be01a17
Diana Badas <diana.badas@gmail.com>
2019-06-10 11:05:11 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,8 +1,5 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @artist_sort_order
-    @song_sort_order
-    @allow_create_artists
-    @allow_create_songs
+  
   end
 end
